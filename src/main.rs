d7ba0653a99c33f6dbae26a8315f32eeb002126d
tomerhc <hacohen0tomer@gmail.com>
2020-04-24 16:18:07 +0300

    added all the pieces

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,14 @@ fn main() {
         white_bisops.push(bisop);
     }
 
+    let mut white_knights= vec![];
+    for i in 0..2{
+        let mut knight = pieces::Knight::new(false);
+        let y = if i == 0 {0} else {7};
+        knight.init(i, board.get_state(pieces::Pos(7, y)));
+        white_knights.push(knight);
+    }
+
     let length  = white_pawns.len();
     for _ in 0..length {
         board.put_piece(Box::new(white_pawns.pop().unwrap()));
@@ -41,6 +49,18 @@ fn main() {
     let length  = white_bisops.len();
     for _ in 0..length {
         board.put_piece(Box::new(white_bisops.pop().unwrap()));
+    }
+
+    let mut white_queen = pieces::Queen::new(false);
+    white_queen.init(0, board.get_state(pieces::Pos(0,5)));
+    board.put_piece(Box::new(white_queen));
+    let mut white_king = pieces::King::new(false);
+    white_king.init(0, board.get_state(pieces::Pos(0,5)));
+    board.put_piece(Box::new(white_king));
+    
+    let length  = white_knights.len();
+    for _ in 0..length {
+        board.put_piece(Box::new(white_knights.pop().unwrap()));
     }
 
     let mut black_pawns = vec![];
@@ -65,6 +85,22 @@ fn main() {
         bisop.init(i, board.get_state(pieces::Pos(0, y)));
         black_bisops.push(bisop);
     }
+
+    let mut black_knights = vec![];
+    for i in 0..2{
+        let mut knight = pieces::Knight::new(true);
+        let y = if i == 0 {0} else {7};
+        knight.init(i, board.get_state(pieces::Pos(0, y)));
+        black_knights.push(knight);
+    }
+
+
+    let mut black_queen = pieces::Queen::new(true);
+    black_queen.init(0, board.get_state(pieces::Pos(7,5)));
+    board.put_piece(Box::new(black_queen));
+    let mut black_king = pieces::King::new(true);
+    black_king.init(0, board.get_state(pieces::Pos(7,5)));
+    board.put_piece(Box::new(black_king));
     
     let length  = black_pawns.len();
     for _ in 0..length {
@@ -79,6 +115,11 @@ fn main() {
     let length  = black_bisops.len();
     for _ in 0..length {
         board.put_piece(Box::new(black_bisops.pop().unwrap()));
+    }
+
+    let length  = black_knights.len();
+    for _ in 0..length {
+        board.put_piece(Box::new(black_knights.pop().unwrap()));
     }
     //let state = board.get_state(pieces::Pos(3,3));
     //let mut bla = vec![];
--- src/pieces.rs
+++ src/pieces.rs
@@ -441,3 +441,379 @@ impl Piece for Bisop {
         self.piece_type.clone()
     }
 }
+
+pub struct Knight {
+    pub color: bool,
+    pub position: Pos,
+    pub legal_moves: Option<Vec<Pos>>,
+    pub piece_type: String,
+}
+
+impl Knight {
+    pub fn new(color:bool) -> Knight {
+        Knight {
+            color: color,
+            position: Pos(0,0),
+            legal_moves: None,
+            piece_type: String::from("knight")
+        }
+    }
+}
+
+
+impl Piece for Knight {
+
+    fn init(&mut self, num: u8, board_state: HashMap<String, Vec<Pos>>){
+        self.position = self.set_init_pos(num);
+        self.legal_moves = self.calc_legal_moves(board_state);
+    }
+
+    fn set_init_pos(&self, num: u8) -> Pos {
+        let y = if num == 0 {1} else {6};
+        match self.color{
+            true => Pos(7, y as u8),
+            false => Pos(0, y as u8)
+        }
+    }
+
+    fn calc_legal_moves(&self, board_state: HashMap<String, Vec<Pos>>) -> Option<Vec<Pos>> {
+        let mut possible = vec![];
+        let Pos(x, y) = self.position;
+        let my_x  = x as i8;
+        let my_y  = y as i8;
+
+        possible.push((my_x -2, my_y+1));
+        possible.push((my_x -2, my_y-1));
+        possible.push((my_x +2, my_y+1));
+        possible.push((my_x +2, my_y-1));
+        possible.push((my_x +1, my_y-2));
+        possible.push((my_x +1, my_y+2));
+        possible.push((my_x -1, my_y-2));
+        possible.push((my_x -1, my_y+2));
+        
+        let final_pos: Vec<Pos> = possible.into_iter().filter(|(x,y)| *x > 0 && *x < 8 && *y > 0 && *y < 8).map(|(x,y)| Pos(x as u8, y as u8)).collect();
+        Some(final_pos) 
+    }
+
+    fn update_legal_moves(&mut self, board_state: HashMap<String, Vec<Pos>>) {
+        self.legal_moves = self.calc_legal_moves(board_state);
+    }
+
+
+    fn move_piece(&mut self, board_state: HashMap<String, Vec<Pos>>, pos: Pos) -> bool{
+        let Pos(x_new, y_new) = pos;
+        match &self.legal_moves {
+            Some(v) => { 
+                if v.iter().any(|&Pos(x,y)| x==x_new && y==y_new){
+                    self.position = pos;
+                    self.legal_moves = self.calc_legal_moves(board_state);
+                    return true;
+                } else {
+                    println!("illigal move");
+                    return false;
+                }
+            },
+            None => {
+                println!("illigal move");
+                return false;
+            }
+        }
+    }
+
+    fn show(&self) -> String{
+        if self.color{
+            String::from("n")
+        }else{
+            String::from("N")
+        }
+    }
+
+    fn get_cur_pos(&self) -> Pos {
+        self.position.clone()
+    }
+
+    fn get_color(&self) -> bool {
+        self.color
+    }
+
+    fn get_piece_type(&self) -> String {
+        self.piece_type.clone()
+    }
+}
+
+pub struct Queen {
+    pub color: bool,
+    pub position: Pos,
+    pub legal_moves: Option<Vec<Pos>>,
+    pub piece_type: String,
+}
+
+impl Queen {
+    pub fn new(color:bool) -> Queen {
+        Queen {
+            color: color,
+            position: Pos(0,0),
+            legal_moves: None,
+            piece_type: String::from("queen")
+        }
+    }
+}
+
+
+impl Piece for Queen {
+
+    fn init(&mut self, num: u8, board_state: HashMap<String, Vec<Pos>>){
+        self.position = self.set_init_pos(num);
+        self.legal_moves = self.calc_legal_moves(board_state);
+    }
+
+    fn set_init_pos(&self, num: u8) -> Pos {
+        match self.color{
+            true => Pos(7, 3),
+            false => Pos(0, 3)
+        }
+    }
+
+    fn calc_legal_moves(&self, board_state: HashMap<String, Vec<Pos>>) -> Option<Vec<Pos>> {
+        let mut possible = vec![];
+        let Pos(my_x, my_y) = self.position;
+        let col_len = board_state.get("col").unwrap().len();
+        let row_len = board_state.get("row").unwrap().len();
+
+        if col_len == 0 {
+            if my_x > 0 {
+                possible.push(Pos(my_x-1, my_y));
+            }
+            if my_x < 7 {
+                possible.push(Pos(my_x+1, my_y));
+            }
+        }
+
+        if row_len == 0 {
+            if my_y > 0 {
+                possible.push(Pos(my_x, my_y-1));
+            }
+            if my_y < 7 {
+                possible.push(Pos(my_x, my_y+1));
+            }
+        }
+
+
+        for (index, i) in board_state.get("col").unwrap().into_iter().enumerate(){
+            let Pos(x,y) = *i;
+            if index == 0 && x > 0 {
+                possible.push(Pos(x-1,y));
+            }
+            if index == col_len-1 && x < 7 {
+                possible.push(Pos(x+1,y));
+            }
+            
+            possible.push(Pos(x,y));
+        }
+        
+        for (index, i) in board_state.get("row").unwrap().into_iter().enumerate(){
+            let Pos(x,y) = *i;
+            if index == 0 && y > 0 {
+                possible.push(Pos(x,y-1));
+            }
+            if index == row_len-1 && y < 7 {
+                possible.push(Pos(x,y+1));
+            }
+            
+            possible.push(Pos(x,y));
+        }
+
+
+        let diag1_len = board_state.get("diag1").unwrap().len();
+        let diag2_len = board_state.get("diag2").unwrap().len();
+
+        if diag1_len == 0 {
+            if my_x > 0 && my_y < 7{
+                possible.push(Pos(my_x-1, my_y+1));
+            }
+            if my_x < 7 && my_y > 0{
+                possible.push(Pos(my_x+1, my_y-1));
+            }
+        }
+
+        if diag2_len == 0 {
+            if my_y > 0 && my_x > 0{
+                possible.push(Pos(my_x-1, my_y-1));
+            }
+            if my_y < 7 && my_x < 7{
+                possible.push(Pos(my_x+1, my_y+1));
+            }
+        }
+
+        for (index, i) in board_state.get("diag1").unwrap().into_iter().enumerate(){
+            let Pos(x,y) = *i;
+            if index == 0 && x > 0 && y < 7 {
+                possible.push(Pos(x-1,y+1));
+            }
+            if index == diag1_len-1 && x < 7 && y > 7 {
+                possible.push(Pos(x+1,y-1));
+            }
+            
+            possible.push(Pos(x,y));
+        }
+        
+        for (index, i) in board_state.get("diag2").unwrap().into_iter().enumerate(){
+            let Pos(x,y) = *i;
+            if index == 0 && y > 0 && x > 0{
+                possible.push(Pos(x-1, y-1));
+            }
+            if index == diag2_len-1 && y < 7 && x < 7{
+                possible.push(Pos(x+1, y+1));
+            }
+            
+            possible.push(Pos(x,y));
+        }
+       
+        
+        Some(possible) 
+    }
+
+    fn update_legal_moves(&mut self, board_state: HashMap<String, Vec<Pos>>) {
+        self.legal_moves = self.calc_legal_moves(board_state);
+    }
+
+
+    fn move_piece(&mut self, board_state: HashMap<String, Vec<Pos>>, pos: Pos) -> bool{
+        let Pos(x_new, y_new) = pos;
+        match &self.legal_moves {
+            Some(v) => { 
+                if v.iter().any(|&Pos(x,y)| x==x_new && y==y_new){
+                    self.position = pos;
+                    self.legal_moves = self.calc_legal_moves(board_state);
+                    return true;
+                } else {
+                    println!("illigal move");
+                    return false;
+                }
+            },
+            None => {
+                println!("illigal move");
+                return false;
+            }
+        }
+    }
+
+    fn show(&self) -> String{
+        if self.color{
+            String::from("q")
+        }else{
+            String::from("Q")
+        }
+    }
+
+    fn get_cur_pos(&self) -> Pos {
+        self.position.clone()
+    }
+
+    fn get_color(&self) -> bool {
+        self.color
+    }
+
+    fn get_piece_type(&self) -> String {
+        self.piece_type.clone()
+    }
+}
+
+pub struct King {
+    pub color: bool,
+    pub position: Pos,
+    pub legal_moves: Option<Vec<Pos>>,
+    pub piece_type: String,
+}
+
+impl King {
+    pub fn new(color:bool) -> King {
+        King {
+            color: color,
+            position: Pos(0,0),
+            legal_moves: None,
+            piece_type: String::from("king")
+        }
+    }
+}
+
+
+impl Piece for King {
+
+    fn init(&mut self, num: u8, board_state: HashMap<String, Vec<Pos>>){
+        self.position = self.set_init_pos(num);
+        self.legal_moves = self.calc_legal_moves(board_state);
+    }
+
+    fn set_init_pos(&self, num: u8) -> Pos {
+        match self.color{
+            true => Pos(7,4),
+            false => Pos(0, 4)
+        }
+    }   
+
+    fn calc_legal_moves(&self, board_state: HashMap<String, Vec<Pos>>) -> Option<Vec<Pos>> {
+        let mut possible = vec![];
+        let Pos(x, y) = self.position;
+        let my_x  = x as i8;
+        let my_y  = y as i8;
+
+        possible.push((my_x-1, my_y));
+        possible.push((my_x+1, my_y));
+        possible.push((my_x, my_y-1));
+        possible.push((my_x, my_y+1));
+        possible.push((my_x+1, my_y+1));
+        possible.push((my_x+1, my_y-1));
+        possible.push((my_x-1, my_y+1));
+        possible.push((my_x-1, my_y-1));
+        
+        let final_pos: Vec<Pos> = possible.into_iter().filter(|(x,y)| *x > 0 && *x < 8 && *y > 0 && *y < 8).map(|(x,y)| Pos(x as u8, y as u8)).collect();
+        Some(final_pos) 
+    }
+
+    fn update_legal_moves(&mut self, board_state: HashMap<String, Vec<Pos>>) {
+        self.legal_moves = self.calc_legal_moves(board_state);
+    }
+
+
+    fn move_piece(&mut self, board_state: HashMap<String, Vec<Pos>>, pos: Pos) -> bool{
+        let Pos(x_new, y_new) = pos;
+        match &self.legal_moves {
+            Some(v) => { 
+                if v.iter().any(|&Pos(x,y)| x==x_new && y==y_new){
+                    self.position = pos;
+                    self.legal_moves = self.calc_legal_moves(board_state);
+                    return true;
+                } else {
+                    println!("illigal move");
+                    return false;
+                }
+            },
+            None => {
+                println!("illigal move");
+                return false;
+            }
+        }
+    }
+
+    fn show(&self) -> String{
+        if self.color{
+            String::from("k")
+        }else{
+            String::from("K")
+        }
+    }
+
+    fn get_cur_pos(&self) -> Pos {
+        self.position.clone()
+    }
+
+    fn get_color(&self) -> bool {
+        self.color
+    }
+
+    fn get_piece_type(&self) -> String {
+        self.piece_type.clone()
+    }
+}
+
